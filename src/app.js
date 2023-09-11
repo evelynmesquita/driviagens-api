5938a1ca2036af2d3654bfd869ed95d9c07f2b4b
Evelyn <evelynbandeiramesquita@gmail.com>
2023-09-11 08:57:44 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
 import dotenv from "dotenv";
+import { errorHandling } from "./middlewares/error.middleware.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(errorHandling);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,11 +1,20 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+import pg from 'pg';
 
-const { Pool } = pg;
+dotenv.config();
+const { DATABASE_URL } = process.env;
 
 const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
+  connectionString: DATABASE_URL
 };
+const { Pool } = pg;
 
 export const db = new Pool(configDatabase);
+
+if (process.env.NODE_ENV === "prod") configDatabase.ssl = true;
+
+db.connect((error, client, done) => {
+  if (error) return console.log('Erro ao conectar ao banco de dados:', error);
+  console.log('Conexão bem-sucedida com o banco de dados PostgreSQL');
+  done();
+});
